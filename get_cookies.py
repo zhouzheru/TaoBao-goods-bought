ac5c6eff0ee892819d64bd392dba48353f99fc29
zhouzheru <48320516+zhouzheru@users.noreply.github.com>
2019-04-21 20:43:23 +0800

    Update get_cookies.py

--- get_cookies.py
+++ get_cookies.py
@@ -24,28 +24,28 @@ class TaoBao():
         self.browser.implicitly_wait(30)
         self.browser.find_element_by_xpath("//a[@class='forget-pwd J_Quick2Static']").click()
         #点击微博登录按钮
-        self.browser.implicitly_wait(30)
+        
         self.browser.find_element_by_xpath("//li[@id='J_OtherLogin']/a").click()
 
         #输入微博账号
-        self.browser.implicitly_wait(30)
+        
         self.browser.find_element_by_xpath("//div[@class='inp username']/input").send_keys(username)
 
         #输入微博密码
-        self.browser.implicitly_wait(30)
+        
         self.browser.find_element_by_xpath("//div[@class='inp password']/input").send_keys(password)
 
         #点击确认按钮
-        self.browser.implicitly_wait(30)
+        
         self.browser.find_element_by_xpath("//div[@class='btn_tip']/a").click()
 
         taobao_name = self.wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='site-nav-user']/a")))
 
         print(taobao_name.text)
-        self.browser.implicitly_wait(30)
+        
         my_taobao = self.browser.find_element_by_xpath("//span[contains(text(),'我的淘宝')]")
         ActionChains(self.browser).move_to_element(my_taobao).perform()
-        self.browser.implicitly_wait(30)
+        
         my_buy_goods = self.browser.find_element_by_xpath("//div[@class='site-nav-menu-bd site-nav-menu-list']/div/a[1]").click()
 
         current_url = self.browser.current_url
